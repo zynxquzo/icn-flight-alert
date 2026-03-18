e3785127fc8e35d44aab566eb454c40b7ff3ca9e
prettymysky-creator <prettymysky@gmail.com>
2026-03-18 21:57:11 +0900

    feat: 도메인별 커스트 예외(Custom Exceptions) 클래스 정의
    -프로젝트 전반에서 사용할 공통 예외 처리를 위한 exceptions.py 모듈 추가
    
    -리소스 부재 시 발생하는 NotFoundException 정의
    
    -유효하지 않은 요청 처리를 위한 BadRequestException 정의
    
    -인천공항 OpenAPI 등 외부 통신 실패 대응을 위한 APIException 정의

---
+++ flight_alert/exceptions.py
@@ -0,0 +1,26 @@
+# flight_alert/exceptions.py
+"""
+Custom Exceptions
+커스텀 예외 클래스
+"""
+
+
+class NotFoundException(Exception):
+    """리소스를 찾을 수 없을 때 발생하는 예외"""
+    def __init__(self, message: str = "Resource not found"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class BadRequestException(Exception):
+    """잘못된 요청일 때 발생하는 예외"""
+    def __init__(self, message: str = "Bad request"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class APIException(Exception):
+    """외부 API 호출 실패 시 발생하는 예외"""
+    def __init__(self, message: str = "External API call failed"):
+        self.message = message
+        super().__init__(self.message)
